382cbaa715d8dc78eb0e8cb53269ac4259a8ab49
BL-Niranjan <niranjan.manivannan@bridgelabz.com>
2026-06-18 16:14:17 +0530

    [Niranjan] Added : UC2 - Room Search & Availability Check

--- BookMyStay/src/InventoryService.java
+++ BookMyStay/src/InventoryService.java
@@ -9,55 +9,11 @@ public class InventoryService {
         roomInventory = new HashMap<>();
     }
 
-    public void addRoomType(String roomType, int count, double price) {
-
-        Room room = new Room(roomType, count, price);
-
-        roomInventory.put(roomType, room);
-
-        System.out.println(roomType + " room added successfully.");
+    public void addRoom(Room room) {
+        roomInventory.put(room.getRoomType(), room);
     }
 
-    public void updateRoomCount(String roomType, int newCount) {
-
-        Room room = roomInventory.get(roomType);
-
-        if (room != null) {
-            room.setAvailableCount(newCount);
-
-            System.out.println(
-                    roomType + " room count updated to " + newCount
-            );
-        } else {
-            System.out.println("Room type not found.");
-        }
-    }
-
-    public void updateRoomPrice(String roomType, double newPrice) {
-
-        Room room = roomInventory.get(roomType);
-
-        if (room != null) {
-            room.setPricePerNight(newPrice);
-
-            System.out.println(
-                    roomType + " room price updated to ₹" + newPrice
-            );
-        } else {
-            System.out.println("Room type not found.");
-        }
-    }
-
-    public void displayAvailability() {
-
-        System.out.println("\n===== ROOM INVENTORY =====");
-
-        for (Room room : roomInventory.values()) {
-            System.out.println(room);
-        }
-    }
-
-    public Room getRoom(String roomType) {
-        return roomInventory.get(roomType);
+    public Map<String, Room> getRoomInventory() {
+        return roomInventory;
     }
 }
--- BookMyStay/src/Room.java
+++ BookMyStay/src/Room.java
@@ -3,11 +3,14 @@ public class Room {
     private String roomType;
     private int availableCount;
     private double pricePerNight;
+    private String amenities;
 
-    public Room(String roomType, int availableCount, double pricePerNight) {
+    public Room(String roomType, int availableCount,
+                double pricePerNight, String amenities) {
         this.roomType = roomType;
         this.availableCount = availableCount;
         this.pricePerNight = pricePerNight;
+        this.amenities = amenities;
     }
 
     public String getRoomType() {
@@ -22,6 +25,10 @@ public class Room {
         return pricePerNight;
     }
 
+    public String getAmenities() {
+        return amenities;
+    }
+
     public void setAvailableCount(int availableCount) {
         this.availableCount = availableCount;
     }
@@ -33,7 +40,8 @@ public class Room {
     @Override
     public String toString() {
         return "Room Type : " + roomType +
-                ", Available Rooms : " + availableCount +
-                ", Price Per Night : ₹" + pricePerNight;
+                "\nAvailable Rooms : " + availableCount +
+                "\nPrice Per Night : ₹" + pricePerNight +
+                "\nAmenities : " + amenities;
     }
 }
